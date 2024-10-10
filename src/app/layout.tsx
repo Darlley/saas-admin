220b667bee44077a8e745ef7b5ba1ee63d14a5a0
Darlley <systembrtt@gmail.com>
2024-10-10 11:37:45 -0400

    fix: navbar and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,5 @@
-import { Button } from '@/components/ui/button';
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import { Sheet, SheetContent, SheetTrigger } from '@/components/ui/sheet';
-import ClientNavigation from '@/constants/client-navigation';
-import Logotipo from '@/icons/Logotipo';
-import { Menu } from 'lucide-react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Link from 'next/link';
 import './globals.css';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -43,87 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className={`${inter.className} relative`}>
-        <div className="w-full fixed top-0 bg-white/20 backdrop-blur-md z-50">
-          <nav className="px-4 2xl:px-0 py-4 container mx-auto flex w-full justify-between">
-            <div className="flex items-center md:gap-x-12">
-              <Link href="#" aria-label="Home">
-                <Logotipo className="h-10 w-auto" />
-              </Link>
-              <div className="hidden md:flex md:gap-x-6">
-                {ClientNavigation.map((item, index) => (
-                  <Link
-                    key={`ClientNavigationDesktop-${index}`}
-                    href={item.path}
-                  >
-                    {item.name}
-                  </Link>
-                ))}
-              </div>
-            </div>
-            <div className="flex items-center gap-x-5 md:gap-x-8">
-              <div className="hidden md:block">
-                <Link href="/login">Entrar</Link>
-              </div>
-              <Button asChild>
-                <Link href="#pricing">
-                  <span>
-                    Começar <span className="hidden lg:inline">agora</span>
-                  </span>
-                </Link>
-              </Button>
-              <div className="-mr-1 md:hidden">
-                <Sheet>
-                  <SheetTrigger asChild>
-                    <Button
-                      variant="outline"
-                      size="icon"
-                      className="shrink-0 md:hidden"
-                    >
-                      <Menu className="h-5 w-5" />
-                      <span className="sr-only">Toggle navigation menu</span>
-                    </Button>
-                  </SheetTrigger>
-                  <SheetContent side="left" className="flex flex-col">
-                    <nav className="flex flex-col gap-2 items-start px-2 text-sm font-medium lg:px-4">
-                      <Link href="/" aria-label="Home">
-                        <Logotipo className="size-8" />
-                      </Link>
-                      {ClientNavigation.map((item, index) => (
-                        <Link
-                          key={`ClientNavigationMobile-${index}`}
-                          href={item.path}
-                          className="flex items-center gap-2 text-lg font-semibold"
-                        >
-                          {item.name}
-                        </Link>
-                      ))}
-                    </nav>
-                    <div className="mt-auto">
-                      <Card>
-                        <CardHeader>
-                          <CardTitle>Upgrade to Pro</CardTitle>
-                          <CardDescription>
-                            Unlock all features and get unlimited access to our
-                            support team.
-                          </CardDescription>
-                        </CardHeader>
-                        <CardContent>
-                          <Button size="sm" className="w-full">
-                            Upgrade
-                          </Button>
-                        </CardContent>
-                      </Card>
-                    </div>
-                  </SheetContent>
-                </Sheet>
-              </div>
-            </div>
-          </nav>
-        </div>
-
-        {children}
-      </body>
+      <body className={`${inter.className} relative`}>{children}</body>
     </html>
   );
 }
--- src/components/PageHome/PageHome.tsx
+++ src/components/PageHome/PageHome.tsx
@@ -1,7 +1,10 @@
+import ClientNavigation from '@/constants/client-navigation';
 import { plans } from '@/constants/plans';
+import Logotipo from '@/icons/Logotipo';
 import { cn } from '@/lib/utils';
-import { CheckCircle } from 'lucide-react';
+import { CheckCircle, Menu } from 'lucide-react';
 import Link from 'next/link';
+
 import {
   Accordion,
   AccordionContent,
@@ -25,12 +28,89 @@ import {
   DialogTitle,
   DialogTrigger,
 } from '../ui/dialog';
+import { Sheet, SheetContent, SheetTrigger } from '../ui/sheet';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '../ui/tabs';
+
 import { PageHomeProps } from './PageHome.types';
 export default function PageHome(props: PageHomeProps) {
   // const {} = props
   return (
     <>
+      <div className="w-full fixed top-0 bg-white/20 backdrop-blur-md z-50">
+        <nav className="px-4 2xl:px-0 py-4 container mx-auto flex w-full justify-between">
+          <div className="flex items-center md:gap-x-12">
+            <Link href="#" aria-label="Home">
+              <Logotipo className="h-10 w-auto" />
+            </Link>
+            <div className="hidden md:flex md:gap-x-6">
+              {ClientNavigation.map((item, index) => (
+                <Link key={`ClientNavigationDesktop-${index}`} href={item.path}>
+                  {item.name}
+                </Link>
+              ))}
+            </div>
+          </div>
+          <div className="flex items-center gap-x-5 md:gap-x-8">
+            <div className="hidden md:block">
+              <Link href="/login">Entrar</Link>
+            </div>
+            <Button asChild>
+              <Link href="#pricing">
+                <span>
+                  Começar <span className="hidden lg:inline">agora</span>
+                </span>
+              </Link>
+            </Button>
+            <div className="-mr-1 md:hidden">
+              <Sheet>
+                <SheetTrigger asChild>
+                  <Button
+                    variant="outline"
+                    size="icon"
+                    className="shrink-0 md:hidden"
+                  >
+                    <Menu className="h-5 w-5" />
+                    <span className="sr-only">Toggle navigation menu</span>
+                  </Button>
+                </SheetTrigger>
+                <SheetContent side="left" className="flex flex-col">
+                  <nav className="flex flex-col gap-2 items-start px-2 text-sm font-medium lg:px-4">
+                    <Link href="/" aria-label="Home">
+                      <Logotipo className="size-8" />
+                    </Link>
+                    {ClientNavigation.map((item, index) => (
+                      <Link
+                        key={`ClientNavigationMobile-${index}`}
+                        href={item.path}
+                        className="flex items-center gap-2 text-lg font-semibold"
+                      >
+                        {item.name}
+                      </Link>
+                    ))}
+                  </nav>
+                  <div className="mt-auto">
+                    <Card>
+                      <CardHeader>
+                        <CardTitle>Upgrade to Pro</CardTitle>
+                        <CardDescription>
+                          Unlock all features and get unlimited access to our
+                          support team.
+                        </CardDescription>
+                      </CardHeader>
+                      <CardContent>
+                        <Button size="sm" className="w-full">
+                          Upgrade
+                        </Button>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </SheetContent>
+              </Sheet>
+            </div>
+          </div>
+        </nav>
+      </div>
+
       <header className="h-svh py-6 mx-auto container flex-1 flex-grow flex flex-col text-center items-center justify-center">
         <h1 className="mx-auto max-w-4xl font-display text-5xl font-medium tracking-tight text-slate-900 sm:text-7xl">
           Faça uma frase de efeito{' '}
@@ -69,11 +149,9 @@ export default function PageHome(props: PageHomeProps) {
               </Button>
             </DialogTrigger>
             <DialogContent className="w-11/12 lg:w-8/12 h-max max-w-full !rounded-none p-0 border-none">
-              <DialogHeader className='p-2 text-sm'>
+              <DialogHeader className="p-2 text-sm">
                 <DialogTitle>Demonstração</DialogTitle>
-                <DialogDescription>
-                  Descrição do video
-                </DialogDescription>
+                <DialogDescription>Descrição do video</DialogDescription>
               </DialogHeader>
               <iframe
                 src="https://www.youtube.com/embed/QXo-Kng7pZQ?autoplay=1&controls=0&quality=hd1080"
@@ -318,14 +396,37 @@ export default function PageHome(props: PageHomeProps) {
         </section>
       </main>
 
-      <footer className="text-center py-10 text-sm">
-        <p>&copy; 2023 Minha Empresa. Todos os direitos reservados.</p>
-        <p>
-          Desenvolvido por{' '}
-          <Link href="https://darlley.dev" className="text-primary font-bold">
-            Qual seu nome?
-          </Link>
-        </p>
+      <footer className="w-full bg-secondary border-t border-slate-400/10">
+        <div className="container mx-auto">
+          <div className="flex flex-col items-center  py-10 sm:flex-row-reverse sm:justify-between">
+            <div className="flex gap-x-6">
+              <Link href="#" className="group" aria-label="TaxPal on X">
+                <svg className="size-6" aria-hidden="true" viewBox="0 0 24 24">
+                  <path d="M13.3174 10.7749L19.1457 4H17.7646L12.7039 9.88256L8.66193 4H4L10.1122 12.8955L4 20H5.38119L10.7254 13.7878L14.994 20H19.656L13.3171 10.7749H13.3174ZM11.4257 12.9738L10.8064 12.0881L5.87886 5.03974H8.00029L11.9769 10.728L12.5962 11.6137L17.7652 19.0075H15.6438L11.4257 12.9742V12.9738Z" />
+                </svg>
+              </Link>
+              <Link href="#" className="group" aria-label="TaxPal on GitHub">
+                <svg className="size-6" aria-hidden="true" viewBox="0 0 24 24">
+                  <path d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0 1 12 6.844a9.59 9.59 0 0 1 2.504.337c1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.02 10.02 0 0 0 22 12.017C22 6.484 17.522 2 12 2Z" />
+                </svg>
+              </Link>
+
+              <p>
+                Desenvolvido por{' '}
+                <Link
+                  href="https://darlley.dev"
+                  className="text-primary font-bold"
+                >
+                  Qual seu nome?
+                </Link>
+              </p>
+            </div>
+            <p className="text-sm">
+              &copy; {new Date().getFullYear()} Microo-SaaS. Todos os direitos
+              reservados.
+            </p>
+          </div>
+        </div>
       </footer>
     </>
   );
