4b4191eb06a44ee58b9935e51db4eb4148dbb8df
Darlley <systembrtt@gmail.com>
2024-10-11 00:31:34 -0400

    feat: role user

--- src/services/auth/index.ts
+++ src/services/auth/index.ts
@@ -1,8 +1,17 @@
-import NextAuth from 'next-auth';
+import NextAuth, { type DefaultSession } from 'next-auth';
 
 import { PrismaAdapter } from '@auth/prisma-adapter';
+import { UserRole } from '@prisma/client';
 import { prisma } from '../database';
 import authConfig from './auth.config';
+
+declare module 'next-auth' {
+  interface Session {
+    user: {
+      role?: UserRole; // Adiciona a propriedade role
+    } & DefaultSession['user'];
+  }
+}
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   ...authConfig,
@@ -16,11 +25,22 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         session.user.id = token?.sub;
       }
 
+      if (token.role && session.user) {
+        session.user.role = token?.role as UserRole;
+      }
+
       return session;
     },
-    jwt: ({ token }) => {
-      console.log({ jwt: token });
-      token.customField = 'Darlley';
+    jwt: async ({ token }) => {
+      if (!token.sub) return token;
+
+      const user = await prisma.user.findUnique({
+        where: { id: token.sub },
+      });
+
+      if (!user) return token;
+      token.role = user?.role;
+
       return token;
     },
   },
